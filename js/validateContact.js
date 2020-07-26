f86caef6faa9aca5599ba00ac797a78ae3d6fe80
Webslingr <jsunnyassy@yahoo.com>
2020-07-26 13:21:47 +0400

    Change hero.scss and added new pages

---
+++ js/validateContact.js
@@ -0,0 +1,57 @@
+function validateName(){
+    if ($("#txtName").val() == ""){
+        $("#txtName").css("border-color", "red");
+        $("#txtNameError").text("Please enter your name");
+        return false;
+    }
+    else{
+        $("#txtName");
+        $("#txtNameError").text("");
+        return true;
+    }
+}
+
+function validateEmail(){
+    if ($("#txtEmail").val() == ""){
+        $("#txtEmail").css("border-color", "red");
+        $("#txtEmailError").text("🐶 \u00E9 Please enter your email");
+        return false;
+    }
+    else{
+        var emailRegEx=/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
+        if (!emailRegEx.test($("#txtEmail").val())){
+            $("#txtEmail").css("border-color", "red");
+            $("#txtEmailError").text("Wrong email format");
+            return false;
+        }
+        else{
+            $("#txtEmail");
+            $("#txtEmailError").text("");
+            return true;
+        }
+    }
+}
+
+function validateMessage(){
+    if ($("#txtMessage").val() == ""){
+        $("#txtMessage").css("border-color", "red");
+        $("#txtMessageError").text("Please enter your message");
+        return false;
+    }
+    else{
+        $("#txtMessage");
+        $("#txtMessageError").text("");
+        return true;
+    }
+}
+
+function validateForm(){
+    validateName()
+    validateEmail()
+    validateMessage()
+    
+    if (validateName() && validateEmail() && validateMessage()){
+        //replace
+        alert("Message sent");
+    }
+}
