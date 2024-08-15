5c33855fe4c19d838f6284f6d2fee25ba9716d24
Webslingr <jsunnyassy@yahoo.com>
2024-08-15 07:15:24 -0300

    Fixed contact form validation

--- js/validateContact.js
+++ js/validateContact.js
@@ -2,12 +2,13 @@ function validateName(){
     if ($("#txtName").val() == ""){
         $("#txtName").css("border-color", "red");
         $("#txtNameError").text("Please enter your name");
-        return false;
+        return true;
     }
     else{
         $("#txtName");
+        $("#txtName").css("border-color", "black");
         $("#txtNameError").text("");
-        return true;
+        return false;
     }
 }
 
@@ -15,7 +16,7 @@ function validateEmail(){
     if ($("#txtEmail").val() == ""){
         $("#txtEmail").css("border-color", "red");
         $("#txtEmailError").text("Please enter your email");
-        return false;
+        return true;
     }
     
     else{
@@ -24,26 +25,52 @@ function validateEmail(){
         if (!emailRegEx.test($("#txtEmail").val())){
             $("#txtEmail").css("border-color", "red");
             $("#txtEmailError").text("Wrong email format");
-            return false;
+            return true;
         }
         else{
             $("#txtEmail");
+            $("#txtEmail").css("border-color", "black");
             $("#txtEmailError").text("");
-            return true;
+            return false;
         }
     }
 }
+
+
+// function validateEmail(){
+//     if ($("#txtEmail").val() == ""){
+//         $("#txtEmail").css("border-color", "red");
+//         $("#txtEmailError").text("Please enter your email");
+//         return false;
+//     }
+    
+//     else{
+//         var emailRegEx=/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
+    
+//         if (!emailRegEx.test($("#txtEmail").val())){
+//             $("#txtEmail").css("border-color", "red");
+//             $("#txtEmailError").text("Wrong email format");
+//             return false;
+//         }
+//         else{
+//             $("#txtEmail");
+//             $("#txtEmailError").text("");
+//             return true;
+//         }
+//     }
+// }
 
 function validateMessage(){
     if ($("#txtMessage").val() == ""){
         $("#txtMessage").css("border-color", "red");
         $("#txtMessageError").text("Please enter your message");
-        return false;
+        return true;
     }
     else{
         $("#txtMessage");
+        $("#txtMessage").css("border-color", "black");
         $("#txtMessageError").text("");
-        return true;
+        return false;
     }
 }
 
@@ -52,8 +79,10 @@ function validateForm(){
     validateEmail()
     validateMessage()
     
-    if (validateName() && validateEmail() && validateMessage()){
+    if (validateName() == false && validateEmail() == false && validateMessage() == false){
         //replace
         alert("Message sent");
+    } else {
+        alert("Message not sent");
     }
 }
